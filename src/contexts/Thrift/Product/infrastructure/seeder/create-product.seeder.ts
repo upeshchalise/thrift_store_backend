7709be85eeea554c4021e72c089495d6a3620195
upeshchalise <upeshchalise@gmail.com>
2024-05-21 22:13:10 +0545

    seeder update

--- src/contexts/Thrift/Product/infrastructure/seeder/create-product.seeder.ts
+++ src/contexts/Thrift/Product/infrastructure/seeder/create-product.seeder.ts
@@ -6,37 +6,37 @@ const seedProducts = [
         id: 'a3411d80-a83e-411e-8409-17170be9899f',
         name: 'furniture',
         price: 10000,
-        imageUrl: 'public/uploads/300614fur.jpg'
+        imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3411d80-a83e-411e-8409-17170be9829f',
         name: 'bike',
         price: 100000,
-        imageUrl: 'public/uploads/istockphoto-452794623.jpg'
+        imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3411d80-a83e-411e-8409-17170be9199f',
         name: 'freeze',
         price: 50000,
-        imageUrl: 'public/uploads/istockphoto-freeze1211554164-2048x2048.jpg'
+        imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3431d80-a83e-411e-8409-17170be9899f',
         name: 'laptop',
         price: 100000,
-        imageUrl: 'public/uploads/istockphotolaptop-178716575-2048x2048.jpg'
+        imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3411d80-a13e-411e-8409-17170be9899f',
         name: 'sofa',
         price: 10000,
-        imageUrl: 'public/uploads/photo-furniture1493663284031-b7e3aefcae8e.jpg'
+        imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3411d80-a83e-411e-8469-17170be9899f',
         name: 'mototcycle',
         price: 100,
-        imageUrl: 'public/uploads/istockphoto-452794623.jpg'
+        imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
 ]
 
