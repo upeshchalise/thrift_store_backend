b7e4eeebbeb51eeaa1fb4a9a0fae99c53023fdc6
upeshchalise <upeshchalise@gmail.com>
2024-05-21 23:58:20 +0545

    product search and pagination

--- src/apps/Thrift/Backend/controllers/products/get-products-by-user-id.controller.ts
+++ src/apps/Thrift/Backend/controllers/products/get-products-by-user-id.controller.ts
@@ -1,14 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import { query } from "express-validator";
 import httpStatus from "http-status";
+import { MESSAGE_CODES } from "../../../../../contexts/Shared/infrastructure/utils/message-codes";
 import { GetProductsByUserIdService } from "../../../../../contexts/Thrift/Product/application/get-produts-by-user-id.service";
 import { Controller } from "../controller";
 
 export class GetProductsByUserIdController implements Controller {
     constructor(private getProductsByUserIdService: GetProductsByUserIdService) {}
+
+    public validate = [
+        query('page').optional().isNumeric().withMessage(MESSAGE_CODES.SHARED.INVALID_PAGE),
+    query('pageSize').optional().isNumeric().withMessage(MESSAGE_CODES.SHARED.INVALID_PAGE_SIZE)
+    ]
     async invoke(req:Request, res: Response, next: NextFunction) : Promise<void> {
         try {
             const {userId} = req.params;
-            const response = await this.getProductsByUserIdService.invoke(userId)
+            const page = Number(req.query.page);
+            const pageSize = Number(req.query.pageSize);
+            const search = req.query.search as string;
+            console.log(req.query);
+            const response = await this.getProductsByUserIdService.invoke(userId,page,pageSize,search)
             res.status(httpStatus.OK).send(response)
         } catch (error) {
             next(error);
--- src/contexts/Thrift/Product/infrastructure/seeder/create-product.seeder.ts
+++ src/contexts/Thrift/Product/infrastructure/seeder/create-product.seeder.ts
@@ -5,36 +5,48 @@ const seedProducts = [
     {
         id: 'a3411d80-a83e-411e-8409-17170be9899f',
         name: 'furniture',
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        quantity: 12,
         price: 10000,
         imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3411d80-a83e-411e-8409-17170be9829f',
         name: 'bike',
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        quantity: 10,
         price: 100000,
         imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3411d80-a83e-411e-8409-17170be9199f',
         name: 'freeze',
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore consectetur adipiscing elit sed do eiusmod tempor incididunt ut labo et dolore magna aliqua.",
+        quantity: 10,
         price: 50000,
         imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3431d80-a83e-411e-8409-17170be9899f',
         name: 'laptop',
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        quantity: 5,
         price: 100000,
         imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3411d80-a13e-411e-8409-17170be9899f',
         name: 'sofa',
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do orem ipsum dolor sit amet consectetur adipiscing e eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        quantity: 3,
         price: 10000,
         imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
     {
         id: 'a3411d80-a83e-411e-8469-17170be9899f',
         name: 'mototcycle',
+        description: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore consectetur adipiscing elit sed do eiusmod tempor incididunt ut labo et dolore magna aliqua.",
+        quantity: 2,
         price: 100,
         imageUrl: 'file-0d3b3ab3-d99b-43e2-ad3f-3ad59f843e6d.png'
     },
@@ -66,6 +78,8 @@ export class CreateProductSeeder {
                         update: {
                             user_id: user.id,
                             name: product.name,
+                            description: product.description,
+                            quantity: product.quantity,
                             price: product.price,
                             imageUrl: product.imageUrl
                         },
@@ -73,6 +87,8 @@ export class CreateProductSeeder {
                             id: product.id,
                             user_id: user.id,
                             name: product.name,
+                            description: product.description,
+                            quantity: product.quantity,
                             price: product.price,
                             imageUrl: product.imageUrl
                         }
